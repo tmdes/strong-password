635ec09d498a0fb54325536f5f308252c90c6aef
Ivan Trofimov <cr.bone@ya.ru>
2016-05-13 00:31:56 +0500

    Methods delivered in prototypes
    Методы и свойства классов Password и Questions вынесены в прототипы.

--- js/questions.js
+++ js/questions.js
@@ -1,71 +1,76 @@
 // объект вопросов
 function Questions(quantity) {
-    // список вопросов
-    var questions = [
-        'Девичья фамилия матери',
-        'Любимый город',
-        'Название любимого озера',
-        'Любимая книга',
-        'Любимый напиток',
-        'Любимое блюдо',
-        'Фамилия первого учителя',
-        'Любимый фильм',
-        'Любимое телевизионное шоу',
-        'Любимое мужское имя',
-        'Любимое женское имя',
-        'Любимая страна',
-        'Ваш кумир'
-    ];
+    // количество заданных вопросов
+    this._quantity = quantity;
 
     // хранит индексы заданных вопросов, чтобы не пересекались
-    var askedQuestionsId = [];
+    this.askedQuestionsId = [];
 
-    // возвращает индекс случайного вопроса
-    function getRandomQuestionId() {
-        return Math.floor(Math.random() * questions.length);
-    }
+    // конструктор
+    this._constructor();
+}
 
-    // возвращает вопрос по порядковому номеру
-    this.get = function (index) {
-        return questions[askedQuestionsId[index]];
-    };
-
-    // изменяет заданный вопрос
-    this.update = function (index) {
-        // текущий ID вопроса
-        var currentQuestionID = askedQuestionsId[index];
-
-        // генерируем новый ID
-        var randQuestionID = getRandomQuestionId();
-
-        // если такой вопрос уже задавали - генерируем новый ID вопроса
-        while ($.inArray(randQuestionID, askedQuestionsId) !== - 1)
-            randQuestionID = getRandomQuestionId();
-
-        // перезаписываем ID текущего вопроса на новый
-        askedQuestionsId[index] = randQuestionID;
-
-        // возвращаем текст вопроса
-        return questions[randQuestionID];
-    };
-
-    // КОНСТРУКТОР //
+// конструктор
+Questions.prototype._constructor = function () {
     // если вопросов в базе меньше чем запрашивает пользователь - возвращаем ошибку
-    if (quantity > questions.length) {
+    if (this._quantity > this._questions.length) {
         console.error("Слишком много вопросов!");
         return;
     }
 
     // выбирает случайные вопросы из массива
-    for (var i = 0; i < quantity; i++) {
+    for (var i = 0; i < this._quantity; i++) {
         // случайный ID вопроса
-        var randQuestionID = getRandomQuestionId();
+        var randQuestionID = this._getRandomQuestionId();
 
         // если такой вопрос уже задавали - генерируем новый ID вопроса
-        while ($.inArray(randQuestionID, askedQuestionsId) !== - 1)
-            randQuestionID = getRandomQuestionId();
+        while ($.inArray(randQuestionID, this.askedQuestionsId) !== -1)
+            randQuestionID = this._getRandomQuestionId();
 
         // добавляет в массив заданных вопросов.
-        askedQuestionsId.push(randQuestionID);
+        this.askedQuestionsId.push(randQuestionID);
     }
-}
+};
+
+// список вопросов
+Questions.prototype._questions = [
+    'Девичья фамилия матери',
+    'Любимый город',
+    'Название любимого озера',
+    'Любимая книга',
+    'Любимый напиток',
+    'Любимое блюдо',
+    'Фамилия первого учителя',
+    'Любимый фильм',
+    'Любимое телевизионное шоу',
+    'Любимое мужское имя',
+    'Любимое женское имя',
+    'Любимая страна',
+    'Ваш кумир'
+];
+
+// возвращает индекс случайного вопроса
+Questions.prototype._getRandomQuestionId = function () {
+    return Math.floor(Math.random() * this._questions.length);
+};
+
+// возвращает вопрос по порядковому номеру
+Questions.prototype.get = function (index) {
+    return this._questions[this.askedQuestionsId[index]];
+};
+
+// изменяет заданный вопрос
+Questions.prototype.update = function (index) {
+    // генерируем новый ID
+    var randQuestionID = this._getRandomQuestionId();
+
+    // если такой вопрос уже задавали - генерируем новый ID вопроса
+    while ($.inArray(randQuestionID, this.askedQuestionsId) !== -1)
+        randQuestionID = this._getRandomQuestionId();
+
+    // перезаписываем ID текущего вопроса на новый
+    this.askedQuestionsId[index] = randQuestionID;
+
+    // возвращаем текст вопроса
+    return this._questions[randQuestionID];
+};
